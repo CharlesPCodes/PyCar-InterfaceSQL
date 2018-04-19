c9344e7b62131312886c3588f73d5476f904613f
Charles Pierson <charles.pierson15@gmail.com>
2018-04-19 19:11:19 -0400

    Cleaned Up Code

--- data.py
+++ data.py
@@ -1,21 +1,17 @@
 from appJar import gui
 import sqlite3
-conn = sqlite3.connect("suspiciouscars.db")
+conn = sqlite3.connect("cars.db")
 c = conn.cursor()
 
 
 #SQL DATA
 
-table_create = "CREATE table if not exists suspiciouscars('Date' TEXT, 'Time' TEXT,  'Car Type' TEXT, 'Car Color' TEXT, 'PlateNum' TEXT, 'Number of People' TEXT, 'People Description' TEXT, 'Police Called' TEXT) ;"
+table_create = "CREATE table if not exists cars('Date' TEXT, 'Time' TEXT,  'Car Type' TEXT, 'Car Color' TEXT, 'PlateNum' TEXT) ;"
 c.execute(table_create)
 
 
 #WINDOW
-app = gui("Sus Cars", "1024x768")
-
-app.addLabel("title", "Welcome to the car database")
-app.setLabelBg("title", "blue")
-
+app = gui("Cars database", "1024x768")
 
 # app.setStretch("both")
 app.setExpand("both")
@@ -29,39 +25,7 @@ app.addLabelEntry("Color", 1,1)
 app.addLabelEntry("PlateNum",2,0)
 app.setEntryDefault("PlateNum", "XXX XXX")
 app.setEntryUpperCase("PlateNum")
-app.addLabelEntry("# of People",2,1)
-app.addLabelEntry("Person(s) Description", 3, 0)
-# app.addLabelOptionBox("Police Called" ,["-Options- ", "Yes", "No"], 3,1)
-app.addLabelEntry("Police Called", 3,1)
-app.setEntryDefault("Police Called", "Y/N")
-app.setEntryUpperCase("Police Called")
-app.addLabelEntry("Search Plate Number", 4,0)
-app.setEntryDefault("Search Plate Number", "XXX XXX")
-app.setEntryUpperCase("Search Plate Number")
-####################################################################
-#TESTING CODE
 
-# def launch(win):
-#     app.showSubWindow(win)
-#     # app.addLabelEntry("License Number")
-
-# # these go in the main window
-# app.addButtons(["License Number"], launch)
-
-# # this is a pop-up
-# app.startSubWindow("License Number", modal=True)
-# app.addMessage(all_rows)
-# app.stopSubWindow()
-
-
-
-
-#TESTING CODE
-
-####################################################################
-
-# def launch(win):
-# 	app.showSubWindow(win)
 
 def press(button):
 	# app.showSubWindow(button)
@@ -76,9 +40,6 @@ def press(button):
 		app.clearEntry("Car Type")
 		app.clearEntry("Color")
 		app.clearEntry("PlateNum")
-		app.clearEntry("# of People")
-		app.clearEntry("Person(s) Description")
-		app.clearEntry("Police Called")
 	
 	
 	elif button == "Search":
@@ -102,19 +63,16 @@ def press(button):
 		userType = app.getEntry("Car Type")
 		userColor= app.getEntry("Color")
 		userPlate = app.getEntry("PlateNum")
-		userNum = app.getEntry("# of People")
-		userDes = app.getEntry("Person(s) Description")
-		police = app.getEntry("Police Called")
 		
 		# print("Date:",date, "Time", time)
-		sql_Statement = "insert into suspiciouscars values (?,?,?,?,?,?,?,?);"
-		c.execute(sql_Statement, (userDate, userTime,  userType, userColor, userPlate, userNum, userDes, police))
+		sql_Statement = "insert into cars values (?,?,?,?,?,?);"
+		c.execute(sql_Statement, (userDate, userTime,  userType, userColor, userPlate))
 		conn.commit()
 		app .infoBox("Sus Cars", "Data succesfully entered" , parent=None)
 
 
 app.addButtons(["Submit", "Clear", "Cancel"], press)
-app.addButtons(["Search", "Clear Search"], press, 4,1)
+app.addButtons(["Search", "Clear Search"], press, 2,1)
 app.setBg("orange")
 app.setFont(16)
 
